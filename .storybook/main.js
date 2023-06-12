0c5df4a184ed02608eddeb22d78ac03aac470ac5
Thadeus Ajayi <ainojie@gmail.com>
2023-06-12 13:21:55 +0100

    initial commit storybook with bare RN

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-actions',
+    '@storybook/addon-react-native-web',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+/**
+ * Native features that won't work on storybook
+ */
